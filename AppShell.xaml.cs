edfc0fcb09d236a23b1722572f9cbe546ce77430
Ruben Rueda <ingeniero.rubenrueda@gmail.com>
2023-12-27 19:26:08 -0500

    FirstCommit
    Implementation Examples From Keyboard Accelerators

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AceleradoresDeTeclado
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace AceleradoresDeTeclado
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void MenuFlyoutItem_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Hola", "Presionaste un acelerador de teclado", "Salir");
+        }
+
+        private void MenuFlyoutItem_Clicked_1(object sender, EventArgs e)
+        {
+            DisplayAlert("Hola", "Presionaste un acelerador de varios modificadores", "Salir");
+        }
+
+        private void MenuFlyoutItem_Clicked_2(object sender, EventArgs e)
+        {
+            DisplayAlert("Hola", "Presionaste un acelerador especifico de la plataforma windows", "Salir");
+        }
+
+        private void MenuFlyoutItem_Clicked_3(object sender, EventArgs e)
+        {
+            DisplayAlert("Hola", "Presionaste la tecla especial F1", "Salir");
+        }
+    }
+
+}
